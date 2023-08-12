112e261f712a38bbade0dfec884698dea8ef61c7
ersumansaurav1991 <ersumansaurav1991@gmail.com>
2023-08-12 18:48:32 +0530

    survey

---
+++ app/src/main/java/com/maxi/dogapi/SurveySubmitActivity.kt
@@ -0,0 +1,11 @@
+package com.maxi.dogapi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SurveySubmitActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_submit_survey)
+    }
+}
